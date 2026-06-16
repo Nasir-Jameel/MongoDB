799dfd0316b397d757a6bd426dd038f6cd588a7e
Nasir-Jameel <beuroairer@gmail.com>
2026-06-16 15:06:48 -0700

    CRUD

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 const express = require("express");
 const app = express();
+const userModel  = require ('./usermodel');
+
 
 app.get('/',function(req,res){
     res.send("hey")
+})
+
+app.get('/create',async (req,res)=>{
+  let createduser = await  userModel.create({              //mongoose ka code bydefaut asyncronous hota ha us k syncronus banany k liya async await ka use kar skty hain.
+        name: "NasirJameelDogarSahib", 
+        username: "NasirDogar11056",
+        email : "nasirdogar@gmail.com"
+    
+    })
+    res.send(createduser)
+})
+app.get('/read',async (req,res)=>{
+    let users =  await userModel.find()
+    res.send(users)
+})
+app.get("/update", async (req, res)=>{
+ let updateduser= await userModel.findOneAndUpdate({name: "Nasir"}, {name:"NasirJameel" }, {returnDocument: "after"} )
+    res.send(updateduser)
+})
+app.get('/delete', async (req, res)=>{
+   let deleteduser=  await userModel.findOneAndDelete({name: "NasirJameelDogarSahib"})  //deleet hony sa pehly 1 bar humain browser wali screen(UI) pa render hota hua nazar ay ga.
+    res.send(deleteduser)
 })
 app.listen(3000);
---
+++ usermodel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb://127.0.0.1:27017/mongopractice`)
+
+const userSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    username: String
+    
+
+})
+module.exports =mongoose.model('user', userSchema);    //module.exports tab use karty hain jab hum routes ('/') ma (CRUD) karna hota ha. (Create,Read,Update,Delete)
